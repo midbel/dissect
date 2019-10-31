4a7f9a00819c082124658cd77d621b216ed13550
midbel <brun.nicolas21@gmail.com>
2019-10-31 07:42:50 +0100

    AST types

---
+++ nodes.go
@@ -0,0 +1,51 @@
+package main
+
+type Node interface {
+	Pos() Position
+}
+
+type Expression interface {
+  Eval(Set) bool
+}
+
+type Value interface{}
+
+type Set struct {
+  values map[string]Value
+}
+
+type Block struct {
+	id    Token
+	nodes []Node
+}
+
+func (b Block) Pos() Position {
+	return b.id.pos
+}
+
+type Parameter struct {
+	id    Token
+	props map[Token]Token
+}
+
+func (p Parameter) Pos() Position {
+	return p.id.pos
+}
+
+type Reference struct {
+	id Token
+}
+
+func (r Reference) Pos() Position {
+	return r.id.pos
+}
+
+type Include struct {
+	pos       Position
+	Predicate Expression
+	nodes     []Node
+}
+
+func (i Include) Pos() Position {
+	return i.pos
+}
