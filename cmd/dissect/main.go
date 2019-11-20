02691f65ddbbe61ccc1934b5f6ba52a360c2543c
midbel <brun.nicolas21@gmail.com>
2019-11-20 20:39:50 +0100

    listen option (only for udp conn for now) - prepare second pass on AST

--- cmd/dissect/main.go
+++ cmd/dissect/main.go
@@ -4,17 +4,56 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"net"
 
 	"github.com/midbel/dissect"
 )
 
 func main() {
+	listen := flag.Bool("l", false, "listen")
 	flag.Parse()
 
-	r, err := os.Open(flag.Arg(0))
+	var err error
+	if *listen {
+		err = dissectFromConn()
+	} else {
+		err = dissectFromFiles()
+	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		os.Exit(3)
+	}
+}
+
+func dissectFromConn() error {
+	r, err := os.Open(flag.Arg(1))
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	a, err := net.ResolveUDPAddr("udp", flag.Arg(0))
+	if err != nil {
+		return err
+	}
+	var c net.Conn
+	if a.IP.IsMulticast() {
+		c, err = net.ListenMulticastUDP("udp", nil, a)
+	} else {
+		c, err = net.ListenUDP("udp", a)
+	}
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	return dissect.Dissect(r, c)
+}
+
+func dissectFromFiles() error {
+	r, err := os.Open(flag.Arg(0))
+	if err != nil {
+		return err
 	}
 	defer r.Close()
 
@@ -22,8 +61,5 @@ func main() {
 	for i := 1; i < flag.NArg(); i++ {
 		files = append(files, flag.Arg(i))
 	}
-	if err := dissect.DissectFiles(r, files); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(2)
-	}
+	return dissect.DissectFiles(r, files)
 }
--- parse.go
+++ parse.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strconv"
 )
 
 const (
@@ -1099,7 +1098,7 @@ func (p *Parser) pushFrame(r io.Reader) error {
 	s, err := Scan(r)
 
 	file := "<input>"
-	if n, ok := r.(interface{ Name() string} ); ok {
+	if n, ok := r.(interface{ Name() string }); ok {
 		file = n.Name()
 	}
 	if err == nil {
@@ -1131,155 +1130,4 @@ func (f *frame) Scan() Token {
 	tok := f.curr
 	f.curr = f.Scanner.Scan()
 	return tok
-}
-
-func merge(dat, root Block) (Node, error) {
-	var nodes []Node
-	for _, n := range dat.nodes {
-		switch n := n.(type) {
-		case Parameter:
-			nodes = append(nodes, n)
-		case Let:
-			nodes = append(nodes, n)
-		case Del:
-			nodes = append(nodes, n)
-		case Seek:
-			nodes = append(nodes, n)
-		case Reference:
-			p, err := root.ResolveParameter(n.id.Literal)
-			if err != nil {
-				return nil, err
-			}
-			nodes = append(nodes, p)
-		case Repeat:
-			r, err := mergeRepeat(n, root)
-			if err != nil {
-				return nil, err
-			}
-			nodes = append(nodes, r)
-		case Match:
-			m, err := mergeMatch(n, root)
-			if err != nil {
-				return nil, err
-			}
-			nodes = append(nodes, m)
-		case Include:
-			b, err := mergeInclude(n, root)
-			if err != nil {
-				return nil, err
-			}
-			if x, ok := b.(Block); ok {
-				nodes = append(nodes, x.nodes...)
-			} else {
-				nodes = append(nodes, b)
-			}
-		default:
-			return nil, fmt.Errorf("unexpected node type %T", n)
-		}
-	}
-	dat = Block{
-		id:    dat.id,
-		nodes: nodes,
-	}
-	return dat, nil
-}
-
-func mergeRepeat(r Repeat, root Block) (Node, error) {
-	if r.repeat.isIdent() {
-		return r, nil
-	}
-	var repeat int64
-	switch r.repeat.Type {
-	case Integer:
-		repeat, _ = strconv.ParseInt(r.repeat.Literal, 0, 64)
-	case Float:
-		f, _ := strconv.ParseFloat(r.repeat.Literal, 64)
-		repeat = int64(f)
-	default:
-		return nil, fmt.Errorf("unexpected token %s", TokenString(r.repeat))
-	}
-	var (
-		dat Block
-		err error
-	)
-	switch n := r.node.(type) {
-	case Reference:
-		dat, err = root.ResolveBlock(n.id.Literal)
-	case Block:
-		dat = n
-	}
-	if err != nil {
-		return nil, err
-	}
-	node, err := merge(dat, root)
-	if err != nil {
-		return nil, err
-	}
-	d, ok := node.(Block)
-	if !ok {
-		return nil, fmt.Errorf("unexpected node type %s", node)
-	}
-	dat = d
-
-	tok := Token{
-		Type:    Keyword,
-		Literal: kwInline,
-		pos:     r.Pos(),
-	}
-	pat := Block{id: tok}
-	for i := int64(0); i < repeat; i++ {
-		pat.nodes = append(pat.nodes, dat.nodes...)
-	}
-	return pat, nil
-}
-
-func mergeMatch(m Match, root Block) (Node, error) {
-	var nodes []MatchCase
-	for _, c := range m.nodes {
-		var (
-			err error
-			dat Block
-		)
-		switch n := c.node.(type) {
-		case Reference:
-			dat, err = root.ResolveBlock(n.id.Literal)
-		case Block:
-			dat = n
-		default:
-			err = fmt.Errorf("unexpected node type %T", n)
-		}
-		if err != nil {
-			return nil, err
-		}
-		c.node, err = merge(dat, root)
-		if err != nil {
-			return nil, err
-		}
-		nodes = append(nodes, c)
-	}
-	m.nodes = nodes
-	return m, nil
-}
-
-func mergeInclude(i Include, root Block) (Node, error) {
-	var (
-		err error
-		dat Block
-	)
-	switch n := i.node.(type) {
-	case Reference:
-		dat, err = root.ResolveBlock(n.id.Literal)
-		if err != nil {
-			return nil, err
-		}
-	case Block:
-		dat = n
-	}
-	if i.node, err = merge(dat, root); err != nil {
-		return nil, err
-	}
-	if i.Predicate == nil {
-		return i.node, err
-	}
-	return i, err
 }
